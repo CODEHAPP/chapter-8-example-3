6713fd396e09947a07b29bd87282429daec59a27
xueyingfeng <job70038@gmail.com>
2024-08-21 18:52:23 +1000

    Add debugging output to push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -3,6 +3,12 @@
 set -e # Exit on any error
 set -u # Treat unset variables as errors
 
+# Debugging output
+echo "CONTAINER_REGISTRY: $CONTAINER_REGISTRY"
+echo "VERSION: $VERSION"
+echo "REGISTRY_UN: $REGISTRY_UN"
+echo "REGISTRY_PW: $REGISTRY_PW"
+
 # Ensure environment variables are set
 : "${CONTAINER_REGISTRY:?CONTAINER_REGISTRY is not set}"
 : "${VERSION:?VERSION is not set}"
